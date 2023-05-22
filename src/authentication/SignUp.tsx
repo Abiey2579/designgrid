8505db4dbe671c6d7a36da995d2e15815dd34a18
Abiey2579 <mbyahya2579@gmail.com>
2023-05-22 20:06:05 +0100

    Yahaya - Completed Sign Up

---
+++ src/authentication/SignUp.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import CoffeeCup from "../assets/svgs/CoffeeCup.svg";
+import AuthButton from "../components/AuthButton";
+
+import Apple from "../assets/svgs/apple-auth.svg";
+import Facebook from "../assets/svgs/facebook-auth.svg";
+import Google from "../assets/svgs/google-auth.svg";
+
+const SignUp = () => {
+  return (
+    <div className="px-24 min-h-screen flex justify-around items-center">
+      <div className="max-w-md">
+        <h2 className="text-dgDarkPurple font-bold text-3xl mb-6">Sign Up</h2>
+        <p className="text-dgDarkPurple text-base mb-6">
+          By signing up you agree to be receiving our newsletters, and updates
+          about design
+        </p>
+        <AuthButton
+          name="Google"
+          icon={Google}
+          label="Sign Up with Google"
+          className="mb-5"
+        />
+        <AuthButton
+          name="Apple"
+          icon={Apple}
+          label="Sign Up with Apple"
+          className="mb-5"
+        />
+        <AuthButton
+          name="Facebook"
+          icon={Facebook}
+          label="Sign Up with Facebook"
+          className="mb-5"
+        />
+        <p className="text-dgDarkPurple text-base mb-6">
+          Already have an account?{" "}
+          <a
+            href="/"
+            className="inline-block text-dgPurple font-bold text-base"
+          >
+            Log In
+          </a>
+        </p>
+      </div>
+      <img src={CoffeeCup} alt="DocsImage" />
+    </div>
+  );
+};
+
+export default SignUp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
 import reportWebVitals from "./reportWebVitals";
-import Home from "./home/Home";
+// import Home from "./home/Home";
+import SignUp from "./authentication/SignUp";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Home />
+    <SignUp />
   </React.StrictMode>
 );
 
