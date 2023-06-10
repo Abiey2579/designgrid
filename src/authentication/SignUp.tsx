dd7722a05e25e28748819cc15746cc0da705f426
Abiey2579 <mbyahya2579@gmail.com>
2023-06-10 03:52:23 +0100

    Yahaya - Removed Appwrite keyword

--- src/authentication/SignUp.tsx
+++ src/authentication/SignUp.tsx
@@ -15,8 +15,8 @@ const SignUp = () => {
     try {
       await account.createOAuth2Session(
         service,
-        `https://appwrite.designgrid.com.ng${uriPaths.ONBOARDING_1}`,
-        `https://appwrite.designgrid.com.ng${uriPaths.SIGN_UP}`
+        `https://designgrid.com.ng${uriPaths.ONBOARDING_1}`,
+        `https://designgrid.com.ng${uriPaths.SIGN_UP}`
       );
     } catch (error) {
       setErrorToast(true);
