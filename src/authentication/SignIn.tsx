d67d4192373cdcea4372c45ac20fcb8420e0fb19
Abiey2579 <mbyahya2579@gmail.com>
2023-07-10 15:45:49 +0100

    Yahaya - Completed Auth

--- src/authentication/SignIn.tsx
+++ src/authentication/SignIn.tsx
@@ -3,108 +3,63 @@ import { Link } from "react-router-dom";
 import { account } from "../assets/config/appwrite-auth";
 import * as uriPaths from "../assets/data/uriPaths";
 import ToastWarning from "../components/ToastWarning";
-import { LOCALHOST } from "../assets/data/constants";
+import { LOCALHOST, DOMAIN } from "../assets/data/constants";
 
 import Google from "./assets/svgs/google.svg";
-import Facebook from "./assets/svgs/facebook.svg";
-import Twitter from "./assets/svgs/twitter.svg";
+import Github from "./assets/svgs/github.svg";
 import LinkedIn from "./assets/svgs/linkedin.svg";
 
-import AuthenticationNavbar from "./components/Navbar";
-
 const SignIn = () => {
-  const [errorToast, setErrorToast] = useState<boolean>(false);
+  const [errorToast, setErrorToast] = useState<string>("");
 
   const handleOAuth = async (service: string) => {
     try {
       await account.createOAuth2Session(
         service,
-        `${LOCALHOST}${uriPaths.DASHBOARD}`,
-        `${LOCALHOST}${uriPaths.SIGN_IN}`
+        `${DOMAIN}${uriPaths.DASHBOARD}`,
+        `${DOMAIN}${uriPaths.SIGN_IN}`
       );
     } catch (error) {
-      setErrorToast(true);
+      setErrorToast("An unexpected error occured");
     }
   };
 
   return (
     <>
       {errorToast && (
-        <ToastWarning
-          title="An unexpected error occurred"
-          close={() => setErrorToast(false)}
-        />
+        <ToastWarning title={errorToast} close={() => setErrorToast("")} />
       )}
-      <AuthenticationNavbar />
-      <div className="w-screen lg:h-screen md:h-full flex justify-center lg:items-center md:items-center lg:mt-0 md:mt-0 mt-[25%]">
-        <div className="w-[400px]">
+      <div className="w-screen h-screen grid place-content-center">
+        <div className="max-w-[400px] w-full">
           {/* AUTHENTICATION FORM HEADER */}
           <div className="w-full text-center">
             <h2 className="font-bold text-[40px] mb-2">Welcome Back</h2>
-            <p className="mb-8">Sign In to your Course Studio account</p>
+            <p className="mb-8">Sign In and Continue with your Learning</p>
           </div>
 
           {/* AUTHENTICATION FORM */}
-          <div className="flex flex-col gap-4 mb-8">
-            <input
-              type="email"
-              className="border border-gray-300 rounded-lg w-full pr-6 py-4 outline-0 placeholder:text-gray-700"
-              placeholder="Email ID"
-            />
-            <input
-              type="password"
-              className="border border-gray-300 rounded-lg w-full pr-6 py-4 outline-0 placeholder:text-gray-700"
-              placeholder="Passcode"
-            />
-            <div>
-              <input
-                type="checkbox"
-                name="rememberMe"
-                className="outline-0 p-2 rounded-md border border-gray-300 mr-3"
-              />
-              <label htmlFor="rememberMe">Remember Me</label>
-            </div>
+          <div className="flex flex-col gap-3 mb-8">
             <button
-              type="submit"
-              className="rounded-lg w-full pr-6 py-4 bg-dgPurple text-dgWhite font-bold"
+              onClick={() => handleOAuth("google")}
+              className="border border-gray-300 rounded-lg flex items-center justify-start px-6 w-full h-[56px]"
             >
-              Sign In
+              <img src={Google} alt="Google" className="w-7 mr-6" />
+              <p className="font-semibold">Continue With Google</p>
             </button>
-            <div className="flex justify-between">
-              <p>Or Sign In with</p>
-              <Link
-                to={uriPaths.FORGOT_PASSCODE}
-                className="font-bold underline text-dgPurple"
-              >
-                Forgot Passcode?
-              </Link>
-            </div>
-            <div className="flex justify-between">
-              <button
-                onClick={() => handleOAuth("facebook")}
-                className="border border-gray-300 rounded-lg flex w-[80px] h-[56px]"
-              >
-                <img src={Facebook} alt="Facebook" className="w-7 m-auto" />
-              </button>
-              <button
-                onClick={() => handleOAuth("google")}
-                className="border border-gray-300 rounded-lg flex w-[80px] h-[56px]"
-              >
-                <img src={Google} alt="Google" className="w-7 m-auto" />
-              </button>
-              <button
-                onClick={() => handleOAuth("linkedin")}
-                className="border border-gray-300 rounded-lg flex w-[80px] h-[56px]"
-              >
-                <img src={LinkedIn} alt="LinkedIn" className="w-7 m-auto" />
-              </button>
-              <button
-                onClick={() => handleOAuth("twitter")}
-                className="border border-gray-300 rounded-lg flex w-[80px] h-[56px]"
-              >
-                <img src={Twitter} alt="Twitter" className="w-7 m-auto" />
-              </button>
-            </div>
+            <button
+              onClick={() => handleOAuth("linkedin")}
+              className="border border-gray-300 rounded-lg flex items-center justify-start px-6 w-full h-[56px]"
+            >
+              <img src={LinkedIn} alt="LinkedIn" className="w-7 mr-6" />
+              <p className="font-semibold">Continue With LinkedIn</p>
+            </button>
+            <button
+              onClick={() => handleOAuth("github")}
+              className="border border-gray-300 rounded-lg flex items-center justify-start px-6 w-full h-[56px]"
+            >
+              <img src={Github} alt="Github" className="w-7 mr-6" />
+              <p className="font-semibold">Continue With Github</p>
+            </button>
           </div>
 
           <p>
--- src/authentication/SignUp.tsx
+++ src/authentication/SignUp.tsx
@@ -3,67 +3,23 @@ import { Link } from "react-router-dom";
 import { account } from "../assets/config/appwrite-auth";
 import * as uriPaths from "../assets/data/uriPaths";
 import ToastWarning from "../components/ToastWarning";
-import { LOCALHOST } from "../assets/data/constants";
-import Spinner from "../components/Spinner";
-import { validateEmail } from "./assets/common/functions";
+import { LOCALHOST, DOMAIN } from "../assets/data/constants";
 
 import Google from "./assets/svgs/google.svg";
-import Facebook from "./assets/svgs/facebook.svg";
-import Twitter from "./assets/svgs/twitter.svg";
+import Github from "./assets/svgs/github.svg";
 import LinkedIn from "./assets/svgs/linkedin.svg";
-
-import AuthenticationNavbar from "./components/Navbar";
 
 const SignUp = () => {
   const [errorToast, setErrorToast] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [passcode, setPasscode] = useState<string>("");
-  const [retype_Passcode, setRetype_Passcode] = useState<string>("");
-  const [spin, setSpin] = useState<boolean>(false);
-
   const handleOAuth = async (service: string) => {
     try {
       await account.createOAuth2Session(
         service,
-        `${LOCALHOST}${uriPaths.ONBOARDING_1}`,
-        `${LOCALHOST}${uriPaths.SIGN_UP}`
+        `${DOMAIN}${uriPaths.ONBOARDING_1}`,
+        `${DOMAIN}${uriPaths.SIGN_UP}`
       );
     } catch (error) {
       setErrorToast("An unexpected error occured");
-    }
-  };
-
-  const handleEmailAuth = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setSpin(true);
-
-    try {
-      // INPUT VALIDATION
-      if (email === "" || passcode === "" || retype_Passcode === "") {
-        throw new Error("All fields are required.");
-      }
-      if (!validateEmail(email)) {
-        throw new Error("Invalid email format.");
-      }
-      if (passcode !== retype_Passcode) {
-        throw new Error("Passcode didn't match");
-      }
-      if (passcode.length < 8 || retype_Passcode.length < 8) {
-        throw new Error("Passcode minimum length is 8 characters");
-      }
-
-      // APPWRITE AUTH API CALL
-      try {
-        const response = await account.createEmailSession(email, passcode);
-        setSpin(false);
-        console.log(response); // Success
-      } catch (error) {
-        setSpin(false);
-        console.log(error as Error); // Failure
-      }
-    } catch (error) {
-      setSpin(false);
-      setErrorToast((error as Error).message);
     }
   };
 
@@ -72,82 +28,38 @@ const SignUp = () => {
       {errorToast && (
         <ToastWarning title={errorToast} close={() => setErrorToast("")} />
       )}
-      <AuthenticationNavbar />
-      <div className="w-screen lg:h-screen md:h-full flex justify-center lg:items-center md:items-center lg:mt-0 md:mt-0 mt-[25%]">
-        <div className="w-[400px]">
+      <div className="w-screen h-screen grid place-content-center">
+        <div className="max-w-[400px] w-full">
           {/* AUTHENTICATION FORM HEADER */}
           <div className="w-full text-center">
             <h2 className="font-bold text-[40px] mb-2">Sign Up Now</h2>
-            <p className="mb-8">Sign Up with DesignGrid Course Studio</p>
+            <p className="mb-8">Sign Up Now to Enroll and Start Learning</p>
           </div>
           {/* AUTHENTICATION FORM */}
-          <form className="flex flex-col gap-4 mb-8" onSubmit={handleEmailAuth}>
-            <input
-              type="email"
-              className="border border-gray-300 rounded-lg w-full pr-6 py-4 outline-0 placeholder:text-gray-700"
-              placeholder="Email ID"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <input
-              type="password"
-              className="border border-gray-300 rounded-lg w-full pr-6 py-4 outline-0 placeholder:text-gray-700"
-              placeholder="Passcode"
-              onChange={(e) => setPasscode(e.target.value)}
-              minLength={8}
-              autoComplete="new-passcode"
-              required
-            />
-            <input
-              type="password"
-              className="border border-gray-300 rounded-lg w-full pr-6 py-4 outline-0 placeholder:text-gray-700"
-              placeholder="Re-type Passcode"
-              onChange={(e) => setRetype_Passcode(e.target.value)}
-              minLength={8}
-              autoComplete="new-passcode"
-              required
-            />
-
+          <div className="flex flex-col gap-3 mb-8">
             <button
-              type="submit"
-              className="rounded-lg w-full pr-6 py-4 bg-dgPurple text-dgWhite font-medium"
+              onClick={() => handleOAuth("google")}
+              className="border border-gray-300 rounded-lg flex items-center justify-start px-6 w-full h-[56px]"
             >
-              {spin ? (
-                <Spinner className="w-6 fill-dgLightPurple text-dgPurple" />
-              ) : (
-                "Sign Up"
-              )}
+              <img src={Google} alt="Google" className="w-7 mr-6" />
+              <p className="font-semibold">Sign Up With Google</p>
             </button>
-            <div className="flex justify-between">
-              <p>Or Sign Up with</p>
-            </div>
-            <div className="flex justify-between">
-              <button
-                onClick={() => handleOAuth("facebook")}
-                className="border border-gray-300 rounded-lg flex w-[80px] h-[56px]"
-              >
-                <img src={Facebook} alt="Facebook" className="w-7 m-auto" />
-              </button>
-              <button
-                onClick={() => handleOAuth("google")}
-                className="border border-gray-300 rounded-lg flex w-[80px] h-[56px]"
-              >
-                <img src={Google} alt="Google" className="w-7 m-auto" />
-              </button>
-              <button
-                onClick={() => handleOAuth("linkedin")}
-                className="border border-gray-300 rounded-lg flex w-[80px] h-[56px]"
-              >
-                <img src={LinkedIn} alt="LinkedIn" className="w-7 m-auto" />
-              </button>
-              <button
-                onClick={() => handleOAuth("twitter")}
-                className="border border-gray-300 rounded-lg flex w-[80px] h-[56px]"
-              >
-                <img src={Twitter} alt="Twitter" className="w-7 m-auto" />
-              </button>
-            </div>
-          </form>
+            <button
+              onClick={() => handleOAuth("linkedin")}
+              className="border border-gray-300 rounded-lg flex items-center justify-start px-6 w-full h-[56px]"
+            >
+              <img src={LinkedIn} alt="LinkedIn" className="w-7 mr-6" />
+              <p className="font-semibold">Sign Up With LinkedIn</p>
+            </button>
+            <button
+              onClick={() => handleOAuth("github")}
+              className="border border-gray-300 rounded-lg flex items-center justify-start px-6 w-full h-[56px]"
+            >
+              <img src={Github} alt="Github" className="w-7 mr-6" />
+              <p className="font-semibold">Sign Up With Github</p>
+            </button>
+          </div>
+
           <p>
             Already have an account?{" "}
             <Link
